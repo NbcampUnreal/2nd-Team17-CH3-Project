9e91099354a3b0ff1f669af5dba6e81214005236
aiden_shin <shinichi9784@gmail.com>
2025-02-20 23:26:28 +0900

    신민웅 : 체력 회복 아이템, 두가지 버전 클래스 생성
    부분 체력회복과 완전 체력회복 아이템 구현
    리플렉션도 반영하여, 에디터에서 수치 조절 가능

---
+++ Source/NXProject/Item/NXBigHealItem.cpp
@@ -0,0 +1,12 @@
+#include "NXBigHealItem.h"
+
+ANXBigHealItem::ANXBigHealItem()
+{
+	HealAmount = 100.0f; //회복량 100으로 설정 (나중에는 체력을 초기화하는 함수를 구현하면 좋을듯)
+	ItemType = "Healing";
+}
+
+void ANXBigHealItem::ActivateItem(AActor* Activator)
+{
+	DestroyItem();
+}
