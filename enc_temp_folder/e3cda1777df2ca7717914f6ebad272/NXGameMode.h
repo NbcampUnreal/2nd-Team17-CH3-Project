eb142ab34e006b84ff2d597aff3509008253b37f
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-20 17:09:14 +0900

    Character EnhancedInputMappingSystem 적용. GameModeBaseClass -> GameModeClass로 변경.

---
+++ enc_temp_folder/e3cda1777df2ca7717914f6ebad272/NXGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "NXGameMode.generated.h"
+
+UCLASS()
+class NXPROJECT_API ANXGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ANXGameMode();
+	
+};
