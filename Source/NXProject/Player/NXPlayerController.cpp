cfcb75d293638ab36aaeec67bb13d5d072ef55d5
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-20 17:09:14 +0900

    Character EnhancedInputMappingSystem 적용. GameModeBaseClass -> GameModeClass로 변경.

--- Source/NXProject/Player/NXPlayerController.cpp
+++ Source/NXProject/Player/NXPlayerController.cpp
@@ -1,2 +1,30 @@
 #include "Player/NXPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+ANXPlayerController::ANXPlayerController()
+	: InputMappingContext(nullptr),
+		MoveAction(nullptr),
+		SprintAction(nullptr),
+		JumpAction(nullptr),
+		LookAction(nullptr),
+		SitAction(nullptr),
+		AttackAction(nullptr),
+		ReloadAction(nullptr)
+{
+}
+
+void ANXPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (InputMappingContext)
+			{
+				Subsystem->AddMappingContext(InputMappingContext, 0);
+			}
+		}
+	}
+}
--- Source/NXProject/Player/NXPlayerController.h
+++ Source/NXProject/Player/NXPlayerController.h
@@ -4,9 +4,34 @@
 #include "GameFramework/PlayerController.h"
 #include "NXPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class NXPROJECT_API ANXPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	ANXPlayerController();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* SprintAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* JumpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* SitAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* AttackAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* ReloadAction;
+
+	virtual void BeginPlay() override;
 	
 };
