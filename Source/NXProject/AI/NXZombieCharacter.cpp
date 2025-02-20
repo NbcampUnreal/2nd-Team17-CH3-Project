9ed7f318b172a21495307df1d9f97b2facc963bc
leesubindlqslek <rftnqls@naver.com>
2025-02-21 02:11:19 +0900

    이수빈 / 좀비, 컨트롤러, 순찰, 추격, 감지 구현

--- Source/NXProject/AI/NXZombieCharacter.cpp
+++ Source/NXProject/AI/NXZombieCharacter.cpp
@@ -9,7 +9,25 @@ ANXZombieCharacter::ANXZombieCharacter()
 	AIControllerClass = ANXZombieAIController::StaticClass();
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 	// ANXZombieCharacter는 
-	// 레벨에 배치되거나 새롭게 생성되면 NXZombieAIController의 빙의가 자동으로 진행됨.
+	// 레벨에 배치되거나 새롭게 생성(스폰)되면 NXZombieAIController의 빙의가 자동으로 진행됨.
 
-	GetCharacterMovement()->MaxWalkSpeed = 300.f;
+}
+
+void ANXZombieCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (false == IsPlayerControlled())
+	{
+		bUseControllerRotationYaw = false;
+
+		GetCharacterMovement()->bOrientRotationToMovement = false;
+		GetCharacterMovement()->bUseControllerDesiredRotation = true;
+		GetCharacterMovement()->RotationRate = FRotator(0.f, 480.f, 0.f);
+
+
+		GetCharacterMovement()->MaxWalkSpeed = 300.f;
+
+
+	}
 }
