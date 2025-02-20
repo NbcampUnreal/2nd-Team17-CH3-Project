a5ee056a8fe110db73c6954dda55ea315caf9b33
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-20 14:53:57 +0900

    SpringArm, CameraComp & PlayerController -> GameMode에 연결.

--- Source/NXProject/Game/NXGameMode.h
+++ Source/NXProject/Game/NXGameMode.h
@@ -1,17 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "NXGameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class NXPROJECT_API ANXGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	ANXGameMode();
 	
 };
