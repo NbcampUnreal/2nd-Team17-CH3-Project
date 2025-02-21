a60420af80cdd83550f640ed1527395ec55cc9ef
inhwaman <inhwa424@naver.com>
2025-02-21 09:36:09 +0900

    이인화: 좀비 브런치 메인 패치풀

--- Source/NXProject/Player/NXPlayerCharacter.h
+++ Source/NXProject/Player/NXPlayerCharacter.h
@@ -7,6 +7,8 @@
 class USpringArmComponent;
 class UCameraComponent;
 
+struct FInputActionValue;
+
 UCLASS()
 class NXPROJECT_API ANXPlayerCharacter : public ANXCharacterBase
 {
@@ -21,7 +23,37 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	UCameraComponent* CameraComp;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float NormalSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float SprintSpeedMultiplier;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	float SprintSpeed;
+
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+	UFUNCTION()
+	void StartJump(const FInputActionValue& Value);
+	UFUNCTION()
+	void StopJump(const FInputActionValue& Value);
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+	UFUNCTION()
+	void StartSprint(const FInputActionValue& Value);
+	UFUNCTION()
+	void StopSprint(const FInputActionValue& Value);
+	UFUNCTION()
+	void StartSit(const FInputActionValue& Value);
+	UFUNCTION()
+	void StopSit(const FInputActionValue& Value);
+	UFUNCTION()
+	void StartAttack(const FInputActionValue& Value);
+	UFUNCTION()
+	void StopAttack(const FInputActionValue& Value);
+	UFUNCTION()
+	void Reload(const FInputActionValue& Value);
 
 };
