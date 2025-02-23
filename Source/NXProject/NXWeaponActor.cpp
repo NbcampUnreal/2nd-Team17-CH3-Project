0bb4addf91234adb033cb8d802f70e59806d4075
Herry-Jun <wnsrlrla0@gmail.com>
2025-02-23 23:15:20 +0900

    김준기: 캐릭터 이동, 점프, 앉기, 달리기 애니메이션 구현 / 총기 부착 구현.

---
+++ Source/NXProject/NXWeaponActor.cpp
@@ -0,0 +1,11 @@
+#include "NXWeaponActor.h"
+
+ANXWeaponActor::ANXWeaponActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);	
+}
+
---
+++ Source/NXProject/NXWeaponActor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NXWeaponActor.generated.h"
+
+UCLASS()
+class NXPROJECT_API ANXWeaponActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ANXWeaponActor();
+
+	USkeletalMeshComponent* GetMesh() const { return Mesh; }
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess))
+	TObjectPtr<USkeletalMeshComponent> Mesh;
+};
