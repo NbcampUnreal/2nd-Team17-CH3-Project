1f95b0ffbd02a7765689b0f53ae8926b9d9e2f4f
inhwaman <inhwa424@naver.com>
2025-02-26 10:57:28 +0900

    이인화: 좀비브랜치 리베이스 후 머지

--- Source/NXProject/AI/NXNonPlayerCharacter.cpp
+++ Source/NXProject/AI/NXNonPlayerCharacter.cpp
@@ -2,6 +2,9 @@
 #include "AI/NXNonPlayerCharacter.h"
 #include "AI/NXAIController.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Components/ProgressBar.h"
+
 
 ANXNonPlayerCharacter::ANXNonPlayerCharacter()
 {
@@ -18,4 +21,61 @@ ANXNonPlayerCharacter::ANXNonPlayerCharacter()
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 480.f, 0.f);
 
 	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+
+	MaxHealth = 100.0f;
+	Health = MaxHealth;
+
+	OverheadWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("OverheadWidget"));
+	OverheadWidget->SetupAttachment(GetMesh());
+	OverheadWidget->SetWidgetSpace(EWidgetSpace::Screen);
+
+	UpdateOverheadHP();
+}
+
+int32 ANXNonPlayerCharacter::GetHealth() const
+{
+	return Health;
+}
+
+void ANXNonPlayerCharacter::UpdateOverheadHP()
+{
+	if (!OverheadWidget)
+	{
+		return;
+	}
+
+	if (UUserWidget* WidgetInstance = OverheadWidget->GetUserWidgetObject())
+	{
+		if (UProgressBar* HPBar = Cast<UProgressBar>(WidgetInstance->GetWidgetFromName(TEXT("OverHeadHPBar"))))
+		{
+			const float HPPercent = (MaxHealth > 0.f) ? Health / MaxHealth : 0.f;
+			HPBar->SetPercent(HPPercent);
+		}
+	}
+}
+
+float ANXNonPlayerCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	Health = FMath::Clamp(Health - DamageAmount, 0.0f, MaxHealth);
+
+	UpdateOverheadHP();
+
+	if (Health <= 0.0f)
+	{
+		OnDeath();
+	}
+	return ActualDamage;
+}
+
+void ANXNonPlayerCharacter::OnDeath()
+{
+	ANXAIController* AIController = Cast<ANXAIController>(GetController());
+	if (true == ::IsValid(AIController))
+	{
+		AIController->EndAI();
+	}
+	Destroy();
+
 }
